c4bb6cbf3428621a3b13e51e9b060258db97bcc4
dayana-sog <dayanasog23@gmail.com>
2020-11-23 14:29:53 +0000

    resolução do desafio 05-menu

--- 5-menu/setup/src/Menu.js
+++ 5-menu/setup/src/Menu.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
-const Menu = () => {
-  return <h2>menu component</h2>;
+const Menu = ({items}) => {
+  return (
+    <div className="section-center"> 
+      {items.map(menuItem => (
+        <article key={menuItem.id} className="menu-item">
+          <img src={menuItem.img} alt={menuItem.name} className="photo"/>
+          <div className="item-info">
+            <header>
+              <h4>{menuItem.title}</h4>
+              <div className="price">$ {menuItem.price}</div>
+            </header>
+            <p className="item-text">{menuItem.desc}</p>
+          </div>
+        </article>
+      ))}
+    </div>
+  );
 };
 
 export default Menu;
