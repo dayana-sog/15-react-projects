e94acf4bee16adc967401a72dfe24bc46fb72a29
dayana-sog <dayanasog23@gmail.com>
2020-12-10 12:34:58 +0000

    resolução do desafio 10/ editItem

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -21,6 +21,18 @@ function App() {
       showAlert(true, 'Please enter value', 'danger');
     } else if (name && isEditing) {
       //deal with edit
+      setList(list.map((item) => {
+        if (item.id === editId) {
+          return {...item, title: name}
+        }
+      
+        return item;
+      }));
+
+      setName('');
+      setEditId(null);
+      setIsEditing(false);
+      showAlert(true, 'item edited', 'success');
     } else {
       //Show the alert
       showAlert(true, `Item ${name} add in your list`, 'success');
@@ -48,6 +60,13 @@ function App() {
     setList(list.filter((item) => item.id !== id));
   };
 
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditId(id);
+    setName(specificItem.title);
+  }; 
+
   return (
     <section className="section-center">
       <div className="grocery-container">
@@ -74,7 +93,11 @@ function App() {
 
         {list.length > 0 && (
           <div className="grocery-container">
-            <List items={list} removeItem={removeItem}/>
+            <List 
+              items={list} 
+              removeItem={removeItem}
+              editItem={editItem}
+            />
             <button 
               className="clear-btn"
               onClick={clearList}
